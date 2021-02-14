80af74810f6c7317acfde69a99a1cb720c2c000b
Eli Ben-Shoshan <eli@benshoshan.com>
2021-02-14 14:42:30 -0500

    Added /nsfp to the app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import requests
 import json
+import random
 
 from flask import abort, Flask, jsonify, request
+from pornhub_api import PornhubApi
 
 app = Flask(__name__)
 url="https://icanhazdadjoke.com/"
@@ -13,6 +15,8 @@ headers = {
 def is_request_valid(request):
     is_token_valid = request.form['token'] == os.environ['token']
     is_team_id_valid = request.form['team_id'] == os.environ['team']
+
+#    print(request.form)
     
     return is_token_valid and is_team_id_valid
 
@@ -28,6 +32,33 @@ def dadjoke():
     else:
         resp = "Something went wrong!"
         
+    return jsonify(
+        response_type='in_channel',
+        text=resp
+    )
+
+@app.route('/nsfp', methods=['POST'])
+def phub():
+    if not is_request_valid(request):
+        abort(400)
+
+    api = PornhubApi()
+
+    if request.form['text'] is not None:
+        search=request.form['text']
+    else:
+        search="boobs"
+
+    results = api.search.search(q=search, ordering="featured", thumbsize="large_hd")
+    vid = random.choice(results.videos)
+    thumbnail = random.choice(vid.thumbs)
+
+    if vid is not None:
+        if vid.title is not None and thumbnail.src is not None and vid.url is not None:
+            resp = "{title} | {link} | {thumbnail}".format(title=vid.title, link=vid.url, thumbnail=thumbnail.src)
+    else:
+        resp = "No response"
+
     return jsonify(
         response_type='in_channel',
         text=resp
