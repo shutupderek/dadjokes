27a57b12eaa0f9b0020d2246829c15b8d665b590
Eli Ben-Shoshan <ebs@ufl.edu>
2023-02-06 15:03:39 -0500

    use the newly created fly.io volume for better persistance.

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ scheduler.start()
 cacheConfig = {
     "CACHE_TYPE": "FileSystemCache",
     "CACHE_DEFAULT_TIMEOUT": 3600,
-    "CACHE_DIR": "/tmp"
+    "CACHE_DIR": "/pickles"
 }
 cache = Cache()
 cache.init_app(app=app, config=cacheConfig)
